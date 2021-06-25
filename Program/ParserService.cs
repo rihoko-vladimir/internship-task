56c6219a61a1b9b17083e340b7690d843bf77519
rihoko-vladimir <vovakozlouskoy@gmail.com>
2021-06-25 21:01:23 +0300

    Finished FieldParser

--- Program/ParserService.cs
+++ Program/ParserService.cs
@@ -1,58 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters;
+using System.Text;
 using Entities;
 
 namespace Program
 {
     public class ParserService : IParserService
     {
-        private string _rawData;
+        private readonly string _rawData;
+
         public ParserService(string rawData)
         {
             _rawData = rawData;
         }
+
         public CalculatedProperties GetParsedData()
         {
-            List<IStopPoint> stopPoints  = _ParsePoints();
-            IFieldDefinition field = _ParseField();
+            var stopPoints = _ParsePoints();
+            var field = _ParseField();
             _VerifyValidility(stopPoints, field);
-            return new CalculatedProperties(field,stopPoints);
+            return new CalculatedProperties(field, stopPoints);
         }
 
         private void _VerifyValidility(List<IStopPoint> stopPoints, IFieldDefinition field)
         {
-            if (field.XSize<0)
-            {
-                throw new InvalidFieldCoordinateException("X coordinate cannot be less than zero");
-            }
+            if (field.XSize < 0) throw new InvalidFieldCoordinateException("X coordinate cannot be less than zero");
 
-            if (field.YSize<0)
+            if (field.YSize < 0) throw new InvalidFieldCoordinateException("Y coordinate cannot be less than zero");
+            foreach (var stopPoint in stopPoints)
             {
-                throw new InvalidFieldCoordinateException("Y coordinate cannot be less than zero");
-            }
-            foreach (IStopPoint stopPoint in stopPoints)
-            {
-                if (stopPoint.XCoordinate>field.XSize)
-                {
+                if (stopPoint.XCoordinate > field.XSize)
                     throw new InvalidCoordinatesException("X coordinate of point is not in the field range");
-                }
 
-                if (stopPoint.YCoordinate>field.YSize)
-                {
+                if (stopPoint.YCoordinate > field.YSize)
                     throw new InvalidCoordinatesException("Y coordinate of point is not in the field range");
-                }
             }
         }
 
         private IFieldDefinition _ParseField()
         {
-            
+            var x = -1;
+            var y = -1;
+            var tempBuilder = new StringBuilder();
+            for (var i = 0; i < _rawData.Length; i++)
+            {
+                var currentCharacter = _rawData[i];
+                if (currentCharacter == 'x')
+                {
+                    int.TryParse(tempBuilder.ToString(), out x);
+                    tempBuilder.Clear();
+                    continue;
+                }
+
+                if (currentCharacter == ' ')
+                {
+                    int.TryParse(tempBuilder.ToString(), out y);
+                    break;
+                }
+
+                tempBuilder.Append(currentCharacter);
+            }
+
+            return new Field(x, y);
         }
 
         private List<IStopPoint> _ParsePoints()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 
@@ -61,7 +75,6 @@ namespace Program
         public InvalidFieldCoordinateException(string xCoordinateCannotBeLessThanZero) : base(
             xCoordinateCannotBeLessThanZero)
         {
-            
         }
     }
 
